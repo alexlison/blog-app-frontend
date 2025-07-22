432ac18734bbc47b9ed1dc82a8055aafb75fded9
alexlison <alexlison910@gmail.com>
2025-07-22 11:02:07 +0530

    created

--- src/Components/CreatePost.jsx
+++ src/Components/CreatePost.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div>
+      <div className="container">
+        <h4 className="m-4 text-center">Post a Message</h4>
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">Post Message</label>
+                <textarea name="" id="" className="input-control"></textarea>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button className="btn btn-success">Post</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
